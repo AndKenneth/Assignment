59e88118db8f3a77cc9d1082f6c29abee80085d1
Kenneth Anderson <ken@kenneth.net.nz>
2014-05-23 22:51:11 +1200

    finished first DFS

--- dfs.java
+++ dfs.java
@@ -1,9 +1,12 @@
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Stack;
 
 
 public class dfs {
@@ -12,7 +15,8 @@ public class dfs {
 		
 		try {
 			System.setIn(new FileInputStream("list.txt"));
-			//System.setOut(new PrintStream(new FileOutputStream("dfs.txt")));
+			System.setOut(new PrintStream(new FileOutputStream("dfs.txt")));
+			//System.setErr(new PrintStream(new FileOutputStream("error.out")));
 			dfs instance = new dfs();
 			instance.start();
 		} catch (FileNotFoundException e) {
@@ -24,29 +28,104 @@ public class dfs {
 
 	private void start() {
 		Kgraph graph = new Kgraph();
-		System.out.print(graph.toString());
+		output(graph.doDFS());
+		
+	}
+	
+	private void output(ArrayList<Integer> list) {
+		Integer[] outputArray = new Integer[list.size()];
+		outputArray = list.toArray(outputArray);
+		 for (int i = 0; i < outputArray.length; i++) System.out.format("%d,%d%n", i, outputArray[i]);
 	}
 	
 	
-	private class Kgraph extends ArrayList<Knode>{
-
+	private class Kgraph {
+		private Knode[] graph;
+		
 		public Kgraph() {
+			ArrayList<Knode> graphList = new ArrayList<Knode>();
 			Scanner in = new Scanner(System.in);
 			while (in.hasNext()) {
-				this.add(new Knode(in.next()));
+				graphList.add(new Knode(in.next()));
 			}
 			in.close();
+			
+			graph = new Knode[graphList.size()];
+			graphList.toArray(graph);
 		}
 		
-		public void doDFS(){
-			ArrayList<Knode> unprocessed = new ArrayList<Knode>();
-			for 
+		private boolean processed() {
+			for (int i = 0; i < graph.length; i++) {
+				if (graph[i].getColor() != Kcolor.BLACK) {
+					return false;
+				}
+			}
+			return true;
+		}
+		
+		public ArrayList<Integer> doDFS(){
+			Stack<Integer> stack = new Stack<Integer>();
+			ArrayList<Integer> processedList = new ArrayList<Integer>();
+			System.err.println("START DO DFS");
+			while (!processed()) {
+				//take lowest unprocessed
+				int current = -1;
+				for (int i = graph.length - 1; i >= 0; i--) {
+					if (graph[i].getColor() == Kcolor.WHITE) {
+						current = i;
+					}
+				}
+				
+				if (current != -1){
+					seen(current, stack);
+				}
+				else break;
+				
+				while (!stack.empty()){
+					current = stack.peek();
+					//add lowest unprocessed child to stack
+					int lowest = Integer.MAX_VALUE;
+					boolean white = false;
+					for (Integer i : graph[current]) {
+						if (indexIsWhite(i)) {
+							lowest = Math.min(lowest, i);
+							white = true;
+						}
+					}
+					if (white) {
+						seen(lowest, stack);
+					} else {
+						process(current, processedList, stack);
+					}
+				}
+				stack.push(-1);
+			}
+			
+			
+			return processedList;
+		}
+		
+		private void seen(int index, Stack<Integer> stack) {
+			stack.push(index);
+			graph[index].setGrey();
+			System.err.println("SEEN Index " + index);
+		}
+		
+		private void process(int index, ArrayList<Integer> list, Stack<Integer> stack) {
+			stack.pop();
+			graph[index].setBlack();
+			list.add(index);
+			System.err.println("PROC Index " + index);
+		}
+		
+		private boolean indexIsWhite(int i) {
+			return graph[i].getColor() == Kcolor.WHITE;
 		}
 		
 		public String toString() {
 			StringBuilder sb = new StringBuilder();
 			int count = 0;
-			for (Knode n : this) {
+			for (Knode n : Arrays.asList(graph)) {
 				sb.append(count);
 				sb.append(": ");
 				sb.append(n.toString());
@@ -66,6 +145,9 @@ public class dfs {
 			
 			this.color = Kcolor.WHITE;
 		}
+		public Kcolor getColor() {
+			return color;
+		}
 		
 		public String getColorString() {
 			switch (color) {
@@ -78,6 +160,14 @@ public class dfs {
 			default:
 				return null;
 			}
+		}
+		
+		public void setGrey() {
+			this.color = Kcolor.GREY;
+		}
+		
+		public void setBlack() {
+			this.color = Kcolor.BLACK;
 		}
 		
 		public String toString() {
